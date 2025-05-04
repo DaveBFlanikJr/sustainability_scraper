d6456a2a9a340b86612c6276c6d55019aa7da929
David Flanik <Daveflanik@gmail.com>
2025-05-04 22:06:30 +0900

    setting up playwrite engine

---
+++ app/services/playwrite_engine.py
@@ -0,0 +1,47 @@
+from playwright.async_api import async_playwright
+
+
+class PlaywrightEngine:
+    def __init__(self, headless: bool = True):
+        self.playwrite = None
+        self.browser = None
+        self.page = None
+        self.headless = headless
+    # start
+    async def start(self):
+        self.playwrite = await async_playwright().start()
+        self.browser = await self.playwrite.chromium.launch(headless=self.headless)
+        self.page = await self.browser.new_page()
+    # navigate
+    async def navigate(self, url):
+        await self.page.goto(url)
+    # click
+    async def click(self, selector: str):
+        await self.page.click(selector)
+    # type
+    async def type(self, selector: str, value):
+        await self.page.fill(selector, value)
+    # select
+    async def select(self, selector: str, value: str):
+        await self.page.select_option(selector, value)
+    # wait for the value
+    async def wait(self, wait_for: str | int):
+        if isinstance(wait_for, int):
+            await self.page.wait_for_timeout(wait_for)
+        if isinstance(wait_for, str):
+            await self.page.wait_for_selector(wait_for)
+
+    async def extract(self, extract_map: dict) -> dict:
+        results = {}
+
+        for key, selector in extract_map.items():
+            try:
+                htmlelement = await self.page.query_selector(selector)
+                text = await htmlelement.inner_text() if htmlelement else None
+                results[key] = text
+            except Exception:
+                results[key] = None
+
+    async def stop(self):
+        await self.browser.close()
+        await self.playwrite.stop()
